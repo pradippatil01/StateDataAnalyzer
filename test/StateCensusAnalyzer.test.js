7c71cf5afd2320ffca07d224680fc3b1565fb60f
pradippatil01 <mr.pradip01@gmail.com>
2021-01-13 18:48:19 +0530

    Add : Negavite test case added for csv count check

--- test/StateCensusAnalyzer.test.js
+++ test/StateCensusAnalyzer.test.js
@@ -5,12 +5,25 @@ const CensusAnalyzer = require('../js/StateCensusAnalyzer');
 const FILE_PATH = './resource/StateCensusData.csv';
 const assert = require('chai').assert;
 
-
 describe("#TestForReadingCsv", () => {
     it("givenStateCensusCsvFile_whenParse_shouldmatchCount", () => {
-        CensusAnalyzer.stateCensusFileLoader(FILE_PATH).then((data) => {
-            assert.lengthOf(data,29);
+        let result = CensusAnalyzer.stateCensusFileLoader(FILE_PATH);
+        result.then((data) => {
+            assert.lengthOf(data, 29);
         })
     })
 
+    it("givenStateCensusCsv_whenCountIsAboveThanRealCount_ShouldNotEqualCheck", () => {
+        let result = CensusAnalyzer.stateCensusFileLoader(FILE_PATH);
+        result.then((data) => {
+            assert.notEqual(data.length, 30);
+        })
+    })
+
+    it("givenStateCensusCsv_whenCountIsBelowThanRealCount_ShouldNotEqualCheck", () => {
+        let result = CensusAnalyzer.stateCensusFileLoader(FILE_PATH);
+        result.then((data) => {
+            assert.notEqual(data.length, 28);
+        })
+    })
 })
